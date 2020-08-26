41cf49668929e9bab71267f83600194da21d3a33
Cameron Shannon <cameroncshannon@gmail.com>
2020-08-26 15:57:32 -0700

    Refactored cbv

--- walkup_effect/payments/views.py
+++ walkup_effect/payments/views.py
@@ -26,11 +26,29 @@ def stripe_config(request):
 
 
 class CheckoutSessionView(View):
-    stripe.api_key = settings.STRIPE_SECRET_KEY
+    # Create new Checkout Session for the order...
+    # -------------------------------------------------------
+    # Other optional params include:
+    #    * [billing_address_collection] - to display billing
+    #        address details on the page.
+    #    * [customer] - if you have an existing Stripe
+    #        Customer ID.
+    #    * [payment_intent_data] - lets capture the payment
+    #        later.
+    #    * [customer_email] - lets you pre-fill the email
+    #        input in the form.
+    # -------------------------------------------------------
+    # For full details see:
+    #   https:#stripe.com/docs/api/checkout/sessions/create
 
+    stripe.api_key = settings.STRIPE_SECRET_KEY
     domain_url = "http://localhost:8000/"
-    success_url = f"{domain_url}payments/success?session_id=" + "{CHECKOUT_SESSION_ID}"
-    cancel_url = domain_url + "payments/cancelled/"
+    checkout_session_str = "{CHECKOUT_SESSION_ID}"
+
+    # ?session_id={CHECKOUT_SESSION_ID} means the redirect will have
+    # the session ID set as a query param
+    success_url = f"{domain_url}payments/success?session_id={checkout_session_str}"
+    cancel_url = f"{domain_url}payments/cancelled/"
 
     @csrf_exempt
     def get(self, request):
@@ -53,49 +71,3 @@ class CheckoutSessionView(View):
 
         except Exception as e:
             return JsonResponse({"error": str(e)})
-
-
-# @csrf_exempt
-# def create_checkout_session(request):
-#     if request.method == "GET":
-#         domain_url = "http://localhost:8000/"
-#         stripe.api_key = settings.STRIPE_SECRET_KEY
-#
-#         try:
-#             # Create new Checkout Session for the order...
-#             # -------------------------------------------------------
-#             # Other optional params include:
-#             #    * [billing_address_collection] - to display billing
-#             #        address details on the page.
-#             #    * [customer] - if you have an existing Stripe
-#             #        Customer ID.
-#             #    * [payment_intent_data] - lets capture the payment
-#             #        later.
-#             #    * [customer_email] - lets you pre-fill the email
-#             #        input in the form.
-#
-#             # For full details see:
-#             #   https:#stripe.com/docs/api/checkout/sessions/create
-#
-#             # ?session_id={CHECKOUT_SESSION_ID} means the redirect will
-#             #   have the session ID set as a query param
-#
-#             checkout_session = stripe.checkout.Session.create(
-#                 success_url=domain_url
-#                 + "payments/success?session_id={CHECKOUT_SESSION_ID}",
-#                 cancel_url=domain_url + "payments/cancelled/",
-#                 payment_method_types=["card"],
-#                 mode="payment",
-#                 line_items=[
-#                     {
-#                         "name": "Donation - Tier 1",
-#                         "quantity": 1,
-#                         "currency": "usd",
-#                         "amount": "5000",  # parses to $50.00
-#                     }
-#                 ],
-#             )
-#             return JsonResponse({"sessionId": checkout_session["id"]})
-#
-#         except Exception as e:
-#             return JsonResponse({"error": str(e)})
